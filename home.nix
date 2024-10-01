98e6bcc8ce295b49d0b188a53b81285352593598
erictrautsch <erictrautsch@outlook.com>
2024-10-01 00:02:33 -0600

    Inital nix setup for dotfiles

---
+++ home.nix
@@ -0,0 +1,111 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./nix/tmux.nix
+  ];
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "erict";
+  home.homeDirectory = "/home/erict";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    # Core
+    pkgs.git
+
+    # Shell of choice
+    pkgs.nushell
+
+    # cmd line utilities
+    pkgs.ripgrep
+    pkgs.bat
+    pkgs.jq
+    pkgs.tree
+    pkgs.tmux
+
+    # Editors
+    pkgs.neovim
+
+    # IaC
+    pkgs.aws-sam-cli
+
+    # Python Stuff
+    pkgs.uv
+
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+    xdg.configFile = {
+    "git" = {
+      source = config.lib.file.mkOutOfStoreSymlink "/home/erict/.config/home-manager/.config/git";
+      recursive = true;
+    };
+    "nix" = {
+      source = config.lib.file.mkOutOfStoreSymlink "/home/erict/.config/home-manager/.config/nix";
+      recursive = true;
+    };
+  };
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/erict/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
---
+++ nix/tmux.nix
@@ -0,0 +1,131 @@
+{ pkgs, ... }:
+let
+  # tmux-super-fingers = pkgs.tmuxPlugins.mkTmuxPlugin {
+  #   pluginName = "tmux-super-fingers";
+  #   version = "unstable-2023-01-06";
+  #   src = pkgs.fetchFromGitHub {
+  #     owner = "artemave";
+  #     repo = "tmux_super_fingers";
+  #     rev = "2c12044984124e74e21a5a87d00f844083e4bdf7";
+  #     sha256 = "sha256-cPZCV8xk9QpU49/7H8iGhQYK6JwWjviL29eWabuqruc=";
+  #   };
+  # };
+in {
+  programs.tmux = {
+    enable = true;
+    historyLimit = 100000;
+    keyMode = "vi";
+    # address vim mode switching delay
+    # https://superuser.com/a/252717/65504
+    escapeTime = 0;
+    plugins = with pkgs;
+      [
+        {
+          plugin = tmuxPlugins.catppuccin;
+          extraConfig = ''
+set -g @catppuccin_flavour 'mocha'
+
+set -g @catppuccin_window_left_separator "█"
+set -g @catppuccin_window_right_separator "█"
+set -g @catppuccin_window_number_position "left"
+set -g @catppuccin_window_middle_separator "█ "
+
+set -g @catppuccin_status_modules_right "host date_time"
+set -g @catppuccin_status_left_separator  ""
+set -g @catppuccin_status_right_separator " "
+set -g @catppuccin_status_right_separator_inverse "yes"
+set -g @catppuccin_status_fill "all"
+set -g @catppuccin_status_connect_separator "no"
+set -g @catppuccin_date_time_text "%H:%M:%S"
+          '';
+        }
+      ];
+    extraConfig = ''
+# Start windows and panes at 1, not 0
+set -g base-index 1
+setw -g pane-base-index 1
+# do not notify me about any activity
+set -g monitor-activity off
+# focus events enabled for terminals that support them
+set -g focus-events on
+# tmux messages are displayed for 3 seconds
+set -g display-time 3000
+set -g status on
+set -g set-titles on
+# interval for refreshing statusline
+set -g status-interval 5
+set -g visual-activity on
+set -g status-position bottom
+# renumber windows when some was deleted
+set -g renumber-windows on
+# terminal window name
+set -g set-titles-string '#{b:pane_current_path}'
+# upgrading current terminal
+set -g terminal-overrides ",xterm-256color:Tc"
+# no spaces between windows in statusline
+set -g window-status-separator ""
+# automatically set current window's name to current directory
+set -g status-interval 1
+set -g automatic-rename on
+set -g automatic-rename-format '#(basename "$(git -C #{pane_current_path} rev-parse --show-toplevel 2>/dev/null || echo "#{pane_current_path}")")'
+
+# matches the outside TERM
+set -g default-terminal "screen-256color"
+
+# unbinding unused keys
+unbind &
+unbind .
+unbind ,
+unbind C-b
+unbind C-n
+unbind C-p
+unbind z
+unbind x
+unbind c
+
+# prefix is C-s
+set -g prefix C-s
+
+# navigating across panes via hjkl
+bind h select-pane -L
+bind j select-pane -D
+bind k select-pane -U
+bind l select-pane -R
+
+# resizing panes via shift+hjkl
+bind -r H resize-pane -L 5
+bind -r J resize-pane -D 5
+bind -r K resize-pane -U 5
+bind -r L resize-pane -R 5
+
+# zoom pane
+bind e resize-pane -Z
+
+# killing current pane with c-k
+bind C-k kill-pane
+
+# new window
+bind c new-window
+
+# move window
+bind M command-prompt "move-window -t '%%'"
+
+# rename window
+bind R command-prompt -I "rename-window #W"
+
+# c-v/c-h to split horizontally/vertically
+bind C-v split-window -h -c "#{pane_current_path}"
+bind C-h split-window -v -c "#{pane_current_path}"
+
+# n/p to move next/previous
+bind n next-window
+bind p previous-window
+
+# inner tmux session prefix
+bind -n C-a send-prefix
+
+# reload tmux configuration
+bind C-e source-file ~/.config/tmux/tmux.conf \; display-message "Reloaded"
+    '';
+  };
+}
